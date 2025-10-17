53455933d6f3c927a11091bc808a5f06848fc644
Jerie <jerie.stolbergrohr@gmail.com>
2025-10-17 14:53:39 +0200

    antal knap fungerer

--- js/product.js
+++ js/product.js
@@ -9,3 +9,24 @@ thumbnails.forEach(t => t.classList.remove(".activated"));
 thumb.classList.add(".activated");
     })
 })
+
+//plus minus knap----------------
+
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const amount = document.querySelector("#amount");
+
+let count = 1;
+
+plus.addEventListener("click", (e) => {
+    e.preventDefault();
+    count++;
+    amount.textContent = count;
+})
+
+minus.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (count > 1) {
+        count--;
+    amount.textContent = count;}
+})
