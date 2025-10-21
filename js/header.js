111c7aa819f01bfa21350947e58322820334cbdb
milene <mimi0003@stud.kea.dk>
2025-10-21 12:08:01 +0200

    rebase

--- js/header.js
+++ js/header.js
@@ -1,34 +1,32 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".navmenu");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove(".active");
     navMenu.classList.remove(".active");
-}))
-
+  })
+);
 
 const catlinks = document.querySelector("#catlinks");
 
-const desiredCategories = [
+/* const desiredCategories = [
   { name: "Makeup", category: "beauty", productId: 3 },
   { name: "Skin Care", category: "skin-care", productId: 118 },
   { name: "Fragrances", category: "fragrances", productId: 10 },
-];
+]; */
 
-function showCategories(categories) {
+/* function showCategories(categories) {
   categories.forEach((category) => {
-    fetch(
-      `https://dummyjson.com/products/category/${category.category}?limit=1`
-    )
+    fetch(`https://dummyjson.com/products/category/${category.category}?limit=1`)
       .then((response) => response.json())
       .then((data) => {
         if (data.products && data.products.length > 0) {
-
           catlinks.innerHTML += `
           <li class="nav-items">
         <a href="produktliste.html?category=${category.category}" class="nav-link">${category.name}</a>
@@ -39,4 +37,4 @@ function showCategories(categories) {
   });
 }
 
-showCategories(desiredCategories);
+showCategories(desiredCategories); */
